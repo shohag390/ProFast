b1d04ff65771d6dfc319c2d19f81881e14bc0ec6
Md Shohag Ali <shohag.webdev@gmail.com>
2026-06-22 22:28:50 +0600

    How It Works page Added

--- src/components/comon/Banner.jsx
+++ src/components/comon/Banner.jsx
@@ -5,7 +5,11 @@ import "swiper/css/pagination";
 import "swiper/css/navigation";
 import { Pagination, Navigation } from "swiper/modules";
 import parcel from "../../assets/tiny-deliveryman.png";
+import parce2 from "../../assets/tiny-deliverycar.png";
+import parce3 from "../../assets/tiny-deliverycar2.png";
 import image1 from "../../assets/big-deliveryman.png";
+import image2 from "../../assets/big-deliveryman2.png";
+import image3 from "../../assets/big-deliveryman3.png";
 
 const Banner = () => {
   return (
@@ -24,27 +28,87 @@ const Banner = () => {
         <SwiperSlide className="w-full">
           <div className="h-[92vh] md:h-[91vh] lg:h-[90vh] 2xl:h-[89vh] w-full flex flex-col lg:flex-row items-center lg:justify-between px-10 lg:px-20">
             <div className="w-full lg:w-1/2">
-              <img src={parcel} alt="" />
-              <h2>
-                We Make Sure Your <br /> <span>Parcel Arrives</span> On Time{" "}
+              <img className="h-20" src={parcel} alt="" />
+              <h2 className="font-bold text-[50px] leading-14 pt-4">
+                We Make Sure Your <br />{" "}
+                <span className="text-[#ACC857]">Parcel Arrives</span> On Time{" "}
                 <br /> - No Fuss.
               </h2>
-              <p>
+              <p className="text-[#606060] pt-6 pb-10">
                 Delivering your parcels safely, quickly, and on schedule. Our
                 trusted logistics team ensures every shipment reaches its
                 destination with speed, care, and complete reliability.
               </p>
-              <div>
-                <button>Get Started</button>
-                <button>Learn More</button>
+              <div className="flex items-center gap-3 lg:gap-5">
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 hidden lg:inline rounded-full text-[14px] lg:text-[16px] hover:bg-[#000000] border border-[#CAEB66] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Learn More
+                </button>
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 rounded-full text-[14px] lg:text-[16px] border border-[#CAEB66] bg-[#CAEB66] hover:bg-[#000000] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Get Started
+                </button>
               </div>
             </div>
-            <div>
-              <img src={image1} alt="" />
+            <div className="w-full lg:w-1/2 lg:flex lg:justify-end">
+              <img className="h-120" src={image1} alt="" />
             </div>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="w-full">Hello</SwiperSlide>
+        <SwiperSlide className="w-full">
+          <div className="h-[92vh] md:h-[91vh] lg:h-[90vh] 2xl:h-[89vh] w-full flex flex-col lg:flex-row items-center lg:justify-between px-10 lg:px-20">
+            <div className="w-full lg:w-1/2">
+              <img className="h-20" src={parce2} alt="" />
+              <h2 className="font-bold text-[50px] leading-14 pt-4">
+                From Pickup to Delivery <br /> We {""}
+                <span className="text-[#ACC857]">Handle Every</span> Parcel
+                <br /> - with Care.
+              </h2>
+              <p className="text-[#606060] pt-6 pb-10">
+                Whether you're sending personal packages or business shipments,
+                and reliable delivery services. Our trusted network ensures
+                every parcel reaches its destination with care and precision.
+              </p>
+              <div className="flex items-center gap-3 lg:gap-5">
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 hidden lg:inline rounded-full text-[14px] lg:text-[16px] hover:bg-[#000000] border border-[#CAEB66] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Learn More
+                </button>
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 rounded-full text-[14px] lg:text-[16px] border border-[#CAEB66] bg-[#CAEB66] hover:bg-[#000000] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Get Started
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:flex lg:justify-end">
+              <img className="h-150" src={image2} alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="w-full">
+          <div className="h-[92vh] md:h-[91vh] lg:h-[90vh] 2xl:h-[89vh] w-full flex flex-col lg:flex-row items-center lg:justify-between px-10 lg:px-20">
+            <div className="w-full lg:w-1/2">
+              <img className="h-20" src={parce3} alt="" />
+              <h2 className="font-bold text-[50px] leading-14 pt-4">
+                Fast, Secure & Reliable <br />{" "}
+                <span className="text-[#ACC857]">Delivery</span> Every Parcel,{" "}
+                <br /> - Every Time.
+              </h2>
+              <p className="text-[#606060] pt-6 pb-10">
+                Experience hassle-free shipping with our professional courier
+                services. We reliability to deliver your parcels on time, giving
+                you complete peace of mind with every shipment.
+              </p>
+              <div className="flex items-center gap-3 lg:gap-5">
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 hidden lg:inline rounded-full text-[14px] lg:text-[16px] hover:bg-[#000000] border border-[#CAEB66] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Learn More
+                </button>
+                <button className="px-4 md:px-6 lg:px-8 2xl:px-10 py-2 lg:py-2.5 rounded-full text-[14px] lg:text-[16px] border border-[#CAEB66] bg-[#CAEB66] hover:bg-[#000000] text-[#000000] hover:text-[#CAEB66] duration-500">
+                  Get Started
+                </button>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:flex lg:justify-end">
+              <img className="h-140" src={image3} alt="" />
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   );
--- src/components/comon/Work.jsx
+++ src/components/comon/Work.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import { IoBookmarks } from "react-icons/io5";
+import { MdOutlineCorporateFare } from "react-icons/md";
+import { PiTruckTrailer } from "react-icons/pi";
+import { TbCashBanknotePlus, TbTruckLoading } from "react-icons/tb";
+
+const work = [
+  {
+    icone: <PiTruckTrailer />,
+    title: "Booking Pick & Drop",
+    description:
+      "Schedule a pickup from your location, and our delivery team will collect and deliver your parcel safely, quickly, and on time.",
+  },
+  {
+    icone: <TbCashBanknotePlus />,
+    title: "Cash On Delivery",
+    description:
+      "Offer your customers a secure Cash on Delivery option while we collect the payment and transfer it to you with complete transparency.",
+  },
+  {
+    icone: <TbTruckLoading />,
+    title: "Delivery Hub",
+    description:
+      "Our strategically located delivery hubs ensure faster parcel processing, efficient routing, and reliable deliveries across the country.",
+  },
+  {
+    icone: <MdOutlineCorporateFare />,
+    title: "Booking SME & Corporate",
+    description:
+      "Tailored logistics solutions for SMEs and corporate businesses with bulk shipping, dedicated support, and cost-effective delivery services.",
+  },
+];
 
 const Work = () => {
-  return <div>Work</div>;
+  return (
+    <div className="px-5 md:px-13 lg:px-25 2xl:px-35 py-8 md:py-12 lg:py-15 2xl:py-18 bg-[#eaeced]">
+      <h4 className="text-[#03373D] text-[30px] lg:text-[35px] font-medium md:font-semibold lg:font-bold pb-5 md:pb-6 lg:pb-7">
+        How It Works
+      </h4>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-5 lg:gap-6">
+        {work?.map((item, index) => (
+          <div className="bg-[#ffff] p-6 md:p-8 lg:p-10 rounded-xl" key={index}>
+            <div className="text-[50px] text-[#03373D]">{item?.icone}</div>
+            <div>
+              <h4 className="text-[18px] md:text-[20px] lg:text-[22px] font-medium lg:font-semibold text-[#03373D] py-1 md:py-2 lg:py-3">
+                {item?.title}
+              </h4>
+              <p className="line-clamp-3 text-[14px] md:text-[16px] text-[#606060] text-justify">
+                {item?.description}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Work;
